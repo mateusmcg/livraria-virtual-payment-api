a89ce903796af0fe2a9d456f59d204c1d778bd1f
Mateus Cerqueira <mateus.mcg@gmail.com>
2018-06-20 21:53:28 -0300

    Alterando porta que a API irá utilizar

--- Program.cs
+++ Program.cs
@@ -14,7 +14,10 @@ namespace livraria_virtual_payment_api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .UseUrls("http://localhost:8003")
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
